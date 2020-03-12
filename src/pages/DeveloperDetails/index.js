129f06a316e14de9a707df73b3b83732325736ac
JasonNelemans <jasonnelemans@hotmail.com>
2020-03-12 22:29:17 +0100

    dispatch action on succes

--- src/pages/DeveloperDetails/index.js
+++ src/pages/DeveloperDetails/index.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { fetchDeveloperById } from "../../store/developerDetails/actions";
 
 export default function DeveloperDetails() {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+
+  useEffect(() => {
+    dispatch(fetchDeveloperById(id));
+  }, [dispatch, id]);
+
   return (
     <div>
       Hello
---
+++ src/store/developerDetails/actions.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+function developerFetchedSuccess(developer) {
+  return {
+    type: "FETCH_DEVELOPER_DETAILS_SUCCESS",
+    payload: developer
+  };
+}
+
+export function fetchDeveloperById(developerId) {
+  return async function(dispatch, getState) {
+    const response = await axios.get(
+      `https://codaisseur-coders-network.herokuapp.com/developers/${developerId}`
+    );
+
+    dispatch(developerFetchedSuccess(response.data));
+  };
+}
